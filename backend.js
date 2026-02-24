ef46e7829fb589d794d6458c1e6e04669fb0e4ba
monika <monikakhanka@gmail.com>
2026-02-24 22:22:24 +0530

    readme

--- backend.js
+++ backend.js
@@ -1,18 +1,18 @@
-const http = require("http");
+  const http = require("http");
 
-const PORT = process.argv[2];
+  const PORT = process.argv[2];
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(
-    JSON.stringify({
-      message: "Response from backend",
-      port: PORT,
-      path: req.url,
-    })
-  );
-});
+  const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "Response from backend",
+        port: PORT,
+        path: req.url,
+      })
+    );
+  });
 
-server.listen(PORT, () => {
-  console.log(`Backend server running on port: ${PORT}`);
-});
+  server.listen(PORT, () => {
+    console.log(`Backend server running on port: ${PORT}`);
+  });
